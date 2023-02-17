530b6641922552d0a2af59e89c98f51e1baed946
ukrmaxim <ukrmaxim@protonmail.com>
2023-02-17 18:30:38 +0200

    Add Readme

--- README.md
+++ README.md
@@ -1,24 +1,79 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Demo version of the crypto exchange.
+Using this application, it is possible to create a bitcoin address on the `testnet` network and use this address to exchange from `USDT` to `BTC`, calculating the commission of the exchanger itself and taking into account the commission of miners (fixed). The current exchange rate is obtained from the public API (according to the schedule and also manually). The balance of a bitcoin address is updated automatically, as well as with each exchange operation.
 
-Things you may want to cover:
+## Ruby and Ruby on Rails version
 
-* Ruby version
+* Ruby 3.1.2
+* Rails 7.0.4
 
-* System dependencies
+## Frontend
 
-* Configuration
+* Hotwire Turbo
+* Hotwire Stimulus
+* Bootstrap
 
-* Database creation
+## Local deployment instructions
 
-* Database initialization
+### Install Docker (Ubuntu)
 
-* How to run the test suite
+* Set up the repository
 
-* Services (job queues, cache servers, search engines, etc.)
+      sudo apt-get update
+      sudo apt-get install ca-certificates curl gnupg lsb-release
 
-* Deployment instructions
+* Add Docker’s official GPG key:
 
-* ...
+      sudo mkdir -p /etc/apt/keyrings
+      curl -fsSL <https://download.docker.com/linux/ubuntu/gpg> | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+      echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] <https://download.docker.com/linux/ubuntu> \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+* Install Docker Engine
+
+      sudo apt-get update
+      sudo chmod a+r /etc/apt/keyrings/docker.gpg
+      sudo apt-get update
+      sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+      sudo docker run hello-world
+
+### Docker build, up, down
+
+* Build or rebuild services
+
+      docker compose build
+
+* Create DB and run migration
+
+      docker compose run --rm web rails db:create
+      docker compose run --rm web rails db:migrate
+
+* Create and start containers
+
+      docker compose up -d
+
+* Stop and remove containers, networks
+
+      docker compose down
+
+### Credentials setup
+
+* Generate Active Record encryption keys
+
+      bin/rails db:encryption:init
+
+* Add keys to your credentials
+
+      EDITOR=nano rails credentials:edit
+
+      secret_key_base:
+
+      active_record_encryption:
+        primary_key:
+        deterministic_key:
+        key_derivation_salt:
+
+      http_auth:
+        name:
+        password:
--- app/services/create_transaction_service.rb
+++ app/services/create_transaction_service.rb
@@ -1,26 +1,31 @@
+# rubocop:disable Metrics/MethodLength
 class CreateTransactionService
   include Bitcoin::Builder
 
   def initialize(amount_get, recip_btc_address)
     @recip_btc_address = recip_btc_address
-    @amount_get = amount_get
-    @address = Setting.find_by(title: 'ex_wallet').value
-    @net_fee = (Setting.find_by(title: 'net_fee').value.to_i / 0.00000001).round(0)
+    @amount_get = (amount_get.to_f / 0.00000001).round(0)
+    @address = Setting.find_by(title: 'ex_wallet')&.value
+    @net_fee = (Setting.find_by(title: 'net_fee')&.value.to_f / 0.00000001).round(0)
+    @wallet_balance = (Wallet.find_by(address: @address)&.balance.to_f / 0.00000001).round(0)
   end
 
   def call
-    Bitcoin.network = :testnet3
+    Bitcoin.network = :testnet
 
-    response = HTTParty.get("https://blockstream.info/testnet/api/address/#{@address}/txs", format: :plain)
-
+    txs = HTTParty.get("https://blockstream.info/testnet/api/address/#{@address}/txs")
     prev_out_index = 0
-    prev_tx = response.first
-    key_base58 = Wallet.find_by(address:).key
+
+    prev_tx_id = txs.first['txid']
+    prev_tx_hex = HTTParty.get("https://blockstream.info/testnet/api/tx/#{prev_tx_id}/hex")
+
+    prev_tx = Bitcoin::P::Tx.new(prev_tx_hex.htb)
+
+    key_base58 = Wallet.find_by(address: @address)&.key
     key = Bitcoin::Key.from_base58(key_base58)
 
     # create a new transaction (and sign the inputs)
     new_tx = build_tx do |t|
-      # add the input you picked out earlier
       t.input do |i|
         i.prev_out prev_tx
         i.prev_out_index prev_out_index
@@ -29,21 +34,19 @@ class CreateTransactionService
 
       # add an output that sends some bitcoins to another address
       t.output do |o|
-        o.value 757 # 0,00000757 BTC in satoshis
-        o.script { |s| s.recipient 'mjnYVFy5x9gwDH2Q12TNy7CP3v8zrtJboB' }
+        o.value @amount_get # BTC in satoshis
+        o.script { |s| s.recipient @recip_btc_address }
       end
 
-      # add another output spending the remaining amount back to yourself
-      # if you want to pay a tx fee, reduce the value of this output accordingly
-      # if you want to keep your financial history private, use a different address
       t.output do |o|
-        o.value 13_400 # 0,00013400 BTC, leave 0.00000600 BTC as fee
+        o.value @wallet_balance - @amount_get - @net_fee # leave @net_fee in satoshi as fee
         o.script { |s| s.recipient key.addr }
       end
     end
 
-    # examine your transaction. you can relay it through http://test.webbtc.com/relay_tx
-    # that will also give you a hint on the error if something goes wrong
-    puts new_tx.to_json
+    hex_tx = Digest::SHA256.hexdigest new_tx.binary_hash
+
+    HTTParty.post('https://blockstream.info/testnet/api/tx', body: hex_tx)
   end
 end
+# rubocop:enable Metrics/MethodLength
