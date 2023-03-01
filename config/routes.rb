c89194fb966bbc2dc26aaec04da96193425d039c
ukrmaxim <ukrmaxim@protonmail.com>
2023-03-01 22:14:21 +0200

    Fix DB seeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   get 'update_rate', action: :update_rate, controller: 'static_pages'
   get 'check_balance', action: :check_balance, controller: 'wallets'
-  # get 'successfully', action: :successfully, controller: 'transactions'
 
   resources :wallets, only: %i[index create update destroy]
   resources :settings, only: %i[index create update destroy]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts 'Seeding...'
-Setting.create([{ title: 'ex_wallet', value: 'your bitcoin address', desc: 'Wallet for exchanger' },
-                { title: 'ex_fee', value: '3', desc: 'Exchange fee (in %)' },
-                { title: 'ex_rate', value: '0.00004046', desc: 'Exchange rate (in BTC)' },
-                { title: 'net_fee', value: '0.00000600', desc: 'Network fee (in BTC)' }])
+Setting.create([{ title: 'ex_wallet', value: 'your bitcoin address', description: 'Wallet for exchanger' },
+                { title: 'ex_fee', value: '3', description: 'Exchange fee (in %)' },
+                { title: 'ex_rate', value: '0.00004046', description: 'Exchange rate (in BTC)' },
+                { title: 'net_fee', value: '0.00000600', description: 'Network fee (in BTC)' }])
 puts 'Seeding done.'
